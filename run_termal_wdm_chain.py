4ebbdedc0fff6e950a26f8dd26a8e1f8a46a2c76
bruno villasenor <bvillasen@gmail.com>
2022-02-14 13:59:38 -0800

    updated

--- run_termal_wdm_chain.py
+++ run_termal_wdm_chain.py
@@ -15,7 +15,7 @@ from uvb_functions import Modify_UVB_Rates_extended
 from data_functions import Write_Solution
 from plot_functions import *
 
-use_mpi = True
+use_mpi = False
 if use_mpi:
   from mpi4py import MPI
   comm = MPI.COMM_WORLD
