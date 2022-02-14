13332bd491f87c9cb39ac4e05117a52d084b11cb
bruno villasenor <bvillasen@gmail.com>
2022-02-14 14:02:38 -0800

    updated

--- tools/data_functions.py
+++ tools/data_functions.py
@@ -2,9 +2,10 @@ import numpy as np
 import h5py as h5
 
 
-def Write_Solution( solution, output_file_name, n_stride=1 ):
+def Write_Solution( solution, output_file_name, n_stride=1, fields_to_write=None ):
   file = h5.File( output_file_name, 'w' )
-  for key in solution:
+  if fields_to_write is None: fields_to_write = solution.keys()
+  for key in fields_to_write:
     file.create_dataset( key, data=solution[key][::n_stride] )
   file.close() 
   print( f'Saved File: {output_file_name}' )
