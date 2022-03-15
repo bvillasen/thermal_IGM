81879ca846854627a11aa4ec91dee4cfb49393a1
bruno villasenor <bvillasen@gmail.com>
2022-03-15 16:10:08 -0700

    updated

--- run_termal_thermal_history_chain.py
+++ run_termal_thermal_history_chain.py
@@ -84,6 +84,6 @@ for sim_id in ids_local:
   solution = Integrate_Evolution( n_H_comov, n_He_comov, T_start, uvb_rates, cosmo, z_start, z_end, n_samples, output_to_file=None, integrator=integrator )
 
   output_file_name = output_dir + f'solution_{sim_id}.h5'
-  Write_Solution( solution, output_file_name, n_stride=1000, fields_to_write=['z', 'temperature'] )
+  Write_Solution( solution, output_file_name, n_stride=1000, fields_to_write=['z', 'temperature', 'n_H', 'n_HI', 'n_e'] )
 
   break
