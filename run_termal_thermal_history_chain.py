214d9af308d6e4771e7d800ad69b45c348a79724
bruno villasenor <bvillasen@gmail.com>
2022-03-15 18:55:41 -0700

    updated

--- run_termal_thermal_history_chain.py
+++ run_termal_thermal_history_chain.py
@@ -70,6 +70,11 @@ print( f'proc_id: {rank}   n_local: {n_local}' )
 
 #Select parameters and compute modified UVB rates
 for sim_id in ids_local:
+  output_file_name = output_dir + f'solution_{sim_id}.h5'
+  if os.path.isfile(output_file_name): 
+    print( f'Skipping {sim_id}' )
+    continue
+  
   sim_params = params_chain[sim_id]
   scale_He, scale_H, delta_z_He, delta_z_H = sim_params
 
@@ -83,6 +88,5 @@ for sim_id in ids_local:
   # integrator = 'rk4'
   solution = Integrate_Evolution( n_H_comov, n_He_comov, T_start, uvb_rates, cosmo, z_start, z_end, n_samples, output_to_file=None, integrator=integrator )
 
-  output_file_name = output_dir + f'solution_{sim_id}.h5'
   Write_Solution( solution, output_file_name, n_stride=50, fields_to_write=['z', 'temperature', 'n_H', 'n_HI', 'n_e'] )
   # Plot_Solution( output_dir, solution, file_name=f'solution_{sim_id}.png' )
